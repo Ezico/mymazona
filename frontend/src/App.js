56cb26fc9b15e81c449e7d44adf40085c1c9edd3
ezico <newage.groupss@gmail.com>
2020-12-04 13:16:03 +0100

    Share Code 2

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,4 @@
+import Product from './components/Product';
 import data from './data'
 
 function App() {
@@ -13,46 +14,12 @@ function App() {
         </div>
         
     </header>
-    <main>
+    <main> 
     <div className="row center">
         {
           data.products.map((product) => (
-
-            <div key={product.name} className="card">
-            <a href={`/product/${product._id}`}>
-            <img className="medium"src={product.image} alt={product.name}></img>
-            
-            </a>
-            <div className="card-body">
-            <a href={`/product/${product._id}`}>
-          <h2>{product.name}</h2>
-                </a>
-                <div className="rating">
-                    <span>
-                        <i className="fa fa-star"></i>
-                    </span>
-                    <span>
-                        <i className="fa fa-star"></i>
-                    </span>
-                    <span>
-                        <i className="fa fa-star"></i>
-                    </span>
-                    <span>
-                        <i className="fa fa-star"></i>
-                    </span>
-                    <span>
-                        <i className="fa fa-star "></i>
-                    </span>
-                </div>
-                <div className="price">
-                    {product.price}
-                </div>
-            </div>
-        </div>
-    
-
-          
-          ))
+                <Product key={product._id}product={product}></Product>
+            ))
           }
         </div>
     </main>
