9a2218ad9f689b13bf407a73179235df085f0a6e
ezico <newage.groupss@gmail.com>
2020-12-04 11:09:39 +0100

    Share Code on Github

---
+++ frontend/src/data.js
@@ -0,0 +1,83 @@
+const data ={
+    products:[
+        {
+            _id:'1',
+            name: 'Nike Slim Shirt',
+            category:'Shirts',
+            image:'/images/p1.jpg',
+            price:120,
+            brand:'Nike',
+            rating: 4.5,
+            numReviews:10,
+            description:'high quality product',
+
+        },
+
+        {
+            _id:'2',
+            name: 'Nike Slim Pant',
+            category:'Pant',
+            image:'/images/p2.jpg',
+            price:120,
+            brand:'Nike',
+            rating: 3.5,
+            numReviews:10,
+            description:'high quality product',
+
+        },
+
+        {
+            _id:'3',
+            name: 'Nike Slim Shirt 2',
+            category:'Shirts',
+            image:'/images/p3.jpg',
+            price:120,
+            brand:'Nike',
+            rating: 5.0,
+            numReviews:110,
+            description:'high quality product',
+
+        },
+
+        {
+            _id:'4',
+            name: 'Nike Slim pant 2',
+            category:'pant',
+            image:'/images/p1.jpg',
+            price:1120,
+            brand:'Nike',
+            rating: 4.5,
+            numReviews:10,
+            description:'high quality product',
+
+        },
+
+        {
+            _id:'5',
+            name: 'Nike trouser Shirt',
+            category:'Shirts',
+            image:'/images/p5.jpg',
+            price:120,
+            brand:'Nike',
+            rating: 4.5,
+            numReviews:10,
+            description:'high quality product',
+
+        },
+
+        {
+            _id:'6',
+            name: 'Nike big Shirt 6',
+            category:'shirt',
+            image:'/images/p6.jpg',
+            price:2120,
+            brand:'Lacoste',
+            rating: 4.5,
+            numReviews:10,
+            description:'high quality product',
+
+        },
+    ],
+};
+
+export default data;
